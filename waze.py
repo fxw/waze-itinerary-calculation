7b207d15560f67fe9cfa758aba429f9dd6362146
Fx Wiplier <francois-xavier.wiplier@publicis.sapient.com>
2020-01-09 16:31:35 +0100

    initial commit

--- waze.py
+++ waze.py
@@ -5,10 +5,11 @@ logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler()
 logger.addHandler(handler)
 
-from_address = '44.7849451,-0.4747446' #'48.7813076,2.2353371'
-to_address = '44.8609818,-0.5541567' #'48.8699547,2.4009557'
+from_address = '44.7849451,-0.4747446'  # '48.7813076,2.2353371'
+to_address = '44.8609818,-0.5541567'  # '48.8699547,2.4009557'
 region = 'EU'
 vehicle_type = 'MOTORCYCLE'
 
-route = WazeRouteCalculator.WazeRouteCalculator(from_address, to_address, region)
+route = WazeRouteCalculator.WazeRouteCalculator(
+    from_address, to_address, region)
 route.calc_all_routes_info(time_delta=-480)
